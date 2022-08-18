a47a004ae0bc7c0780468e21bce89fd7d374934c
Juan Johnson <ericrenee21@gmail.com>
2022-08-17 22:20:44 -0400

    Refactor the Display Objects

--- quiz.js
+++ quiz.js
@@ -1,116 +1,110 @@
-var questionDisplayNumber = document.querySelector('.question-active .question-number');
+
 var questionCount = document.getElementsByClassName("question-number").length;
 var questionDisplayTotal = document.querySelector(".question-active .question-total");
 var questionBodies = document.getElementsByClassName("question-body");
-var answersArray = {
-    question_group_1: 'c',
-    question_group_2: 'd',
-    question_group_3: 'd'
-}
-//console.log(questionDisplayNumber);
-//console.log(questionCount);
-//console.log(questionDisplayTotal);
-//console.log(questionBodies);
+var navButtons = document.getElementsByClassName("nav-button");
+var radioButtons = document.getElementsByClassName("question-input");
 
 questionDisplayTotal.textContent = questionCount;
 
-var navButtons = document.getElementsByClassName("nav-button");
+
 for(var navButton of navButtons) {
-  navButton.addEventListener('click', function(event) {
-    questionDisplayNumber = document.querySelector('.question-active .question-number').textContent;
-    //console.log(event.target.textContent.toLowerCase().replace(" ","").replace(">","").replace("<",""));
+    navButton.addEventListener('click', function(event) {
     var direction = event.target.textContent.toLowerCase().replace(" ","").replace(">","").replace("<","");
-    //console.log(direction);    
 
-    if(direction === "next") {
-      var nextQuestion = String(Number(questionDisplayNumber) + 1);
-      //console.log(nextQuestion);
+        if(direction === "next") {
+        var questionDisplayNumber = document.querySelector('.question-active .question-number');
+        var nextQuestion = (Number(questionDisplayNumber.textContent) + 1);
+        var totalQuestions = (Number(questionDisplayTotal.textContent));
+        if(nextQuestion <= totalQuestions) {
+            for(var questionBody of questionBodies) {
+                if(questionBody.children["1"].firstElementChild.textContent === questionDisplayNumber.textContent) {
+                questionBody.classList.remove('question-active');
+                }
+                if(questionBody.children["1"].firstElementChild.textContent === String(nextQuestion)) {
+                questionBody.classList.add('question-active');
+                }
+            }
+        }
+        if(nextQuestion === totalQuestions) {
+            document.querySelector(".nav-next").textContent = "*****";
+            document.querySelector(".nav-back").textContent = "< Back";
 
-      for(var questionBody of questionBodies) {
-        //console.log(questionBody.children["1"].firstElementChild.textContent);
-        if(questionBody.children["1"].firstElementChild.textContent === questionDisplayNumber) {
-          questionBody.classList.remove('question-active');
+        } else {
+            document.querySelector(".nav-next").textContent = "Next >"
+            document.querySelector(".nav-back").textContent = "< Back"
         }
-        if(questionBody.children["1"].firstElementChild.textContent === nextQuestion) {
-          questionBody.classList.add('question-active');
-        }
-      }
-      if(nextQuestion === questionDisplayTotal.textContent) {
-        document.querySelector(".nav-next").textContent = "Submit";
-      }
-
-    } 
-    else if (direction === "back") {
-      var nextQuestion = String(Number(questionDisplayNumber) - 1);
-      //console.log(nextQuestion);
-      //console.log(typeof(nextQuestion));
-      if(nextQuestion !== "0"){
-        for(var questionBody of questionBodies) {
-          console.log(questionBody.children["1"].firstElementChild.textContent);
-          if(questionBody.children["1"].firstElementChild.textContent === questionDisplayNumber) {
-            questionBody.classList.remove('question-active');
-          }
-          if(questionBody.children["1"].firstElementChild.textContent === nextQuestion) {
-            questionBody.classList.add('question-active');
-          }
-        }
-        if(nextQuestion !== questionDisplayTotal.textContent) {
-          document.querySelector(".nav-next").textContent = "Next >";
         } 
-      }
-    
-    } 
-    else if (direction === "submit") {
-      var radioButtons = document.getElementsByClassName("question-input")
-      //console.log(radioButtons);
-      var counter = 0;
-      var correctAnswers = 0;
-      var resultsArray = {};
-      var previousQuestion = "q01";
-      var currentQuestion = "q01";
-      var checkedFlag = false;
-      for(radioButton of radioButtons) {
-        if(radioButton.checked === true) {
-          counter++;
-          //console.log(counter, radioButton.value);
-          if(radioButton.value === "choice-1c") {
-            correctAnswers++;
-          }
-          if(radioButton.value === "choice-2d") {
-            correctAnswers++;
-          }
-          if(radioButton.value === "choice-3d") {
-            correctAnswers++;
-          }
+        else if (direction === "back") {
+        var questionDisplayNumber = document.querySelector('.question-active .question-number');
+        var nextQuestion = (Number(questionDisplayNumber.textContent) - 1);
+        if(nextQuestion !== 0){
+            for(var questionBody of questionBodies) {
+            if(questionBody.children["1"].firstElementChild.textContent === questionDisplayNumber.textContent) {
+                questionBody.classList.remove('question-active');
+            }
+            if(questionBody.children["1"].firstElementChild.textContent === String(nextQuestion)) {
+                questionBody.classList.add('question-active');
+            }
+            }
+            if(nextQuestion !== questionDisplayTotal.textContent) {
+            document.querySelector(".nav-next").textContent = "Next >";
+            } 
         }
+        if(nextQuestion === 1) {
+            document.querySelector(".nav-next").textContent = "Next >"
+            document.querySelector(".nav-back").textContent = "*****"
+        } else {
+            document.querySelector(".nav-next").textContent = "Next >"
+            document.querySelector(".nav-back").textContent = "< Back"
+        }
+        
+        } 
+        else if (direction === "submit") {
 
-        //console.log(radioButton.name, radioButton.value, radioButton.checked);
-        currentQuestion = radioButton.name;
-        if(previousQuestion !== currentQuestion) {
-            console.log("Question Change Point");
+
+            //Array check for correct answer variables 
+            var correctObjectAnswers = 0 
+            var resultsObject = {};
+            const answerObject = {
+                q01: "choice-1c",
+                q02: "choice-2d",
+                q03: "choice-3d"
+            }
+            var previousQuestion = "q01";
+            var currentQuestion = "q01";
+            var checkedFlag = false;
+            for(radioButton of radioButtons) {
+            
+                //Array check for correct answer procedure  
+                //console.log(radioButton.name, radioButton.value, radioButton.checked);
+                currentQuestion = radioButton.name;
+                if(previousQuestion !== currentQuestion) {
+                    //console.log("Question Change Point");
+                    if(checkedFlag === false) {
+                        resultsObject[previousQuestion] = "";
+                    }
+                    checkedFlag = false;
+                }
+                previousQuestion = radioButton.name;
+                if(radioButton.checked === true) {
+                    resultsObject[radioButton.name] = radioButton.value;
+                    checkedFlag = true;
+                }
+            }
+
             if(checkedFlag === false) {
-                resultsArray[previousQuestion] = "";
+                resultsObject[previousQuestion] = "";
             }
             checkedFlag = false;
-        }
-        previousQuestion = radioButton.name;
 
-        if(radioButton.checked === true) {
-            resultsArray[radioButton.name] = radioButton.value;
-            checkedFlag = true;
-        }
-
-      }
-      console.log("Question Change Point");
-      if(checkedFlag === false) {
-          resultsArray[previousQuestion] = "";
-      }
-      checkedFlag = false;
-      
-      console.log(resultsArray);
-      //console.log(`Correct Answers: ${correctAnswers} out of ${questionCount}`);
-      document.querySelector('.nav-indicator').textContent = `Your Score: ${correctAnswers} out of ${questionCount}`;
-    }
-  })
+            for (var key in resultsObject) {
+                if(resultsObject[key] === answerObject[key]) {
+                    correctObjectAnswers++;
+                }
+            }
+            document.querySelector('.nav-indicator').textContent = `Your Score: ${correctObjectAnswers} out of ${questionCount}`;
+        }               
+    })
 }
     
